15a8242a123465822df42ee3622de10a79cebdeb
Tim Bradt <tjbradt@mtu.edu>
2019-05-15 22:52:13 -0400

    Add a Generator for Creating Random Structs

--- src/RPGHelper.jl
+++ src/RPGHelper.jl
@@ -1,9 +1,25 @@
 module RPGHelper
 
 import Random
-export roll, meanroll
+export roll, meanroll, Generator
+
 include("diceroll.jl")
 include("tables.jl")
+
+struct Generator{T,TA}
+	tables::TA
+	function Generator{T}(v::NamedTuple) where T
+		tables = merge(NamedTuple(), Dict(map(fieldnames(T)) do f
+			f => v[f]
+		end))
+		new{T,typeof(tables)}(tables)
+	end
+end
+
+Generator{T}(;kwargs...) where T = Generator{T}(merge(NamedTuple(),Dict(kwargs)))
+
+Base.propertynames(gen::Generator) = collect(keys(getfield(gen,:tables)))
+Base.getproperty(gen::Generator, f::Symbol) = getproperty(getfield(gen,:tables), f)
 
 for d in (2, 4, 6, 8, 10, 12, 20, 100)
 	@eval begin
@@ -19,9 +35,13 @@ roll(rng::Random.AbstractRNG, d::AbstractDie, ::Val{true}) = sum(roll(rng, d, Va
 roll(rng::Random.AbstractRNG, d::AbstractDie, ::Val{false}) = rand(rng, d)
 
 roll(rng::Random.AbstractRNG, tab::Table) = roll(rng, tab.table[roll(rng, tab.die)[1]])
+roll(rng::Random.AbstractRNG, gen::Generator{T}) where T = T(map(x->roll(rng,x), getfield(gen,:tables))...,)
+
 meanroll(v) = v
 meanroll(d::AbstractDie; roundFunc = x->floor(Int,x)) = roundFunc( (maxroll(d) + minroll(d)) / 2 )
 
 meanroll(tab::Table) = meanroll(tab.table[meanroll(tab.die)])
+
+meanroll(gen::Generator{T}) where T = T(map(meanroll, getfield(gen,:tables))...,)
 
 end # module
