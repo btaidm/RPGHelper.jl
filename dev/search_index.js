61bcd28c96c6610a29d96bf1837b60b20f2252d4
Documenter.jl <documenter@juliadocs.github.io>
2020-12-29 18:53:27 +0000

    build based on 9793ced

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RPGHelper","category":"page"},{"location":"#RPGHelper","page":"Home","title":"RPGHelper","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RPGHelper]","category":"page"}]
+}
