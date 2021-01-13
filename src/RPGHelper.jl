76d89c4f1ced8b74edfd726fd92a6d51247edc82
Tim Bradt <tjbradt@mtu.edu>
2021-01-13 08:50:17 -0500

    Don't treat vectors different right now

--- src/RPGHelper.jl
+++ src/RPGHelper.jl
@@ -9,7 +9,6 @@ include("tables.jl")
 
 roll(v, args...; kwargs...) = roll(Random.GLOBAL_RNG, v, args...; kwargs...)
 roll(::AbstractRNG, x) = x
-roll(rng::AbstractRNG, x::Vector) = rand(rng, x)
 Base.:!(d) = roll(d)
 
 export minroll, maxroll, meanroll, roll
